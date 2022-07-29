6501c08482e168814e47b20d33ae3835a6092547
ter-9001 <gabrielmartinsgmgm23@gmail.com>
2022-07-29 19:46:50 -0300

    skill

--- index.js
+++ index.js
@@ -16,13 +16,81 @@ Array.from(document.getElementsByClassName('allprogress')).forEach(
 
         
     }
+)
 
 
+/*
 
+Array.from().forEach(
 
+    (obj) => 
+    {
+        obj.onclick( (obj) => {  alert('olá'); } );
+    }
 )
 
 
 
+function arrow(target,a)
+{
+    alert('olá')
+}
+
+*/
 
 
+Array.from(document.getElementsByClassName('fieldheader')).forEach(
+
+    obj =>
+    {
+        obj.onclick = ()   => { 
+            let dropdown = obj.querySelector("[name='arrow-dropdown']");
+            let dropup = obj.querySelector("[name='arrow-dropup']");
+
+
+
+             if(dropdown!=null)
+               { 
+                        dropdown.setAttribute('name', 'arrow-dropup');
+
+                        let skills = obj.nextElementSibling;
+                        
+                        skills.classList.remove('off');
+                        skills.classList.add('on');
+                     
+                        skills.scrollIntoView();
+
+                        
+                        //window.scrollBy(0, parseInt(skills.style.width));
+
+                        return;
+
+                }
+
+
+             if(dropup!=null)
+              {
+                dropup.setAttribute('name', 'arrow-dropdown');
+
+                     let skills = obj.nextElementSibling;
+                        
+                     skills.classList.remove('on');
+                        
+                     skills.classList.add('off');
+
+                     return;
+                     
+                        
+              }
+
+
+
+
+
+        }
+    }
+
+
+
+
+    );
