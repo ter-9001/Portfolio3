a1eed94ebae29fa677dc2c357767375c58d6c867
ter-9001 <gabrielmartinsgmgm23@gmail.com>
2022-07-30 22:40:47 -0300

    menu

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
-/*var number = document.getElementById('test').closest('.skill');
+var mobile;
 
-
-
-*/
+if(window.screen.width < 400)
+{
+    mobile=true;
+}
+else
+{
+    mobile = false;
+}
 
 Array.from(document.getElementsByClassName('allprogress')).forEach(
 
@@ -17,26 +22,6 @@ Array.from(document.getElementsByClassName('allprogress')).forEach(
         
     }
 )
-
-
-/*
-
-Array.from().forEach(
-
-    (obj) => 
-    {
-        obj.onclick( (obj) => {  alert('olá'); } );
-    }
-)
-
-
-
-function arrow(target,a)
-{
-    alert('olá')
-}
-
-*/
 
 
 Array.from(document.getElementsByClassName('fieldheader')).forEach(
@@ -94,3 +79,74 @@ Array.from(document.getElementsByClassName('fieldheader')).forEach(
 
 
     );
+
+
+window.addEventListener('resize', changeScreen);
+
+function changeScreen()
+{
+    if(window.screen.width < 400)
+    {
+        mobile=true;
+    }
+    else
+    {
+        mobile = false;
+    }
+}    
+
+
+    window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  
+      if(mobile)
+      {
+        let nav = document.getElementById('apresentation').querySelector('nav');
+
+        let asmenutiny = document.getElementById('asmenutiny');
+
+
+
+                if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
+                {
+
+
+                    nav.display  = 'none';
+                    asmenutiny.style.display  = 'flex';
+
+                
+                }
+                else
+                {
+
+                    
+                                        
+                    asmenutiny.style.display  = 'none';
+                    nav.style.display ='flex';
+
+
+                    
+                    
+                }
+
+                console.log(asmenutiny.style.display);
+                
+      }
+      else
+      {
+
+        let button = document.getElementById('upbutton') ;
+                if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) 
+                {
+
+                    button.style.display = 'block';
+
+
+                }
+                else
+                {
+                    button.style.display = 'none';
+                }
+      }
+}
